82aa859c076eb65736d6ed69935000dc3301a9ed
moham1009 <hn19999@bristol.ac.uk>
2020-03-10 17:26:02 +0000

    Chanved

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/Ai.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/Ai.java
@@ -5,7 +5,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import javax.annotation.Nonnull;
 
 /**
- * All CPU (Ai) players should implement this interface and be on the classpath. hi my name is mohammedl
+ * All CPU (Ai) players should implement this interface and be on the classpath. hi my name is mohammed
 public interface Ai {
 
 	/**
