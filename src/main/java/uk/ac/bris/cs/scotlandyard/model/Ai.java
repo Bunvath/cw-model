7febb3993fe183287c0bc4b4ee18501806fc06d4
lybunvath <lybunvath@gmail.com>
2020-03-10 17:22:31 +0000

    Changed

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/Ai.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/Ai.java
@@ -5,7 +5,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import javax.annotation.Nonnull;
 
 /**
- * All CPU (Ai) players should implement this interface and be on the classpath. hi my name is ly bunvathhdj
+ * All CPU (Ai) players should implement this interface and be on the classpath. hi my name is ly bunvathh
 public interface Ai {
 
 	/**
